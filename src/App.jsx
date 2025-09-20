b8eafd916474324e10b5c81287581081f3d5c241
Krishna Purohith <purohithkrishna.1@gmail.com>
2025-09-20 19:03:32 +0530

    jsx rules

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import Add from "./components/Add";
 import Footer from "./components/Footer";
 import Greet from "./components/Greet";
 import Header from "./components/Header";
+import JSXRues from "./components/JSXRues";
 import MainContent from "./components/MainContent";
 import WelcomeMessage from "./components/WelcomeMessage";
 
 const App = () => {
-  return <WelcomeMessage />;
+  return (
+    <div className="">
+      <JSXRues />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/JSXRues.jsx
@@ -0,0 +1,14 @@
+const JSXRues = () => {
+  return (
+    <div>
+      <h1 className="">JSX Rules</h1>
+      <p>JSX must return a Single parent element</p>
+      <p>JSX elements must be properly</p>
+      <p>
+        JSX attributes are written using CamelCase(e.g., className instead of
+        class
+      </p>
+    </div>
+  );
+};
+export default JSXRues;
